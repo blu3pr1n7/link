034fae3e9098e58705cddb6012f2caf3c104dfa9
drax <mars@atlas.local>
2024-03-02 00:33:27 +0000

    update docs & entrypoint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_markdown_builder']
+extensions = [
+    'sphinx_markdown_builder',
+    'sphinx.ext.autodoc'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
